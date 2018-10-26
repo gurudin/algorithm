28f44c274a671ca5d0892be300e7ffa4a5729483
gaoxiang <hc.gaoxiang@gmail.com>
2018-10-26 15:47:57 +0800

    fixed twoSum for go

---
+++ 两数之和/demo.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func twoSum(nums []int, target int) []int {
+	result := []int{0, 0}
+	for i := 0; i < len(nums); i++ {
+		for j := (i + 1); j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				result[0] = i
+				result[1] = j
+
+				return result
+			}
+		}
+	}
+
+	return result
+}
+
+func twoSumB(nums []int, target int) []int {
+	result := []int{0, 0}
+	tmp := make(map[string]int)
+	for i := 0; i < len(nums); i++ {
+		tmp[strconv.Itoa(nums[i])] = i
+	}
+
+	for i := 0; i < len(nums); i++ {
+		_, ok := tmp[strconv.Itoa(target-nums[i])]
+		if ok && tmp[strconv.Itoa(target-nums[i])] != i {
+			result[0] = i
+			result[1] = tmp[strconv.Itoa(target-nums[i])]
+
+			return result
+		}
+	}
+
+	return result
+}
+
+func main() {
+	nums := []int{3, 2, 95, 4, -3}
+	target := 92
+	fmt.Println(twoSum(nums, target))
+	fmt.Println(twoSumB(nums, target))
+}
